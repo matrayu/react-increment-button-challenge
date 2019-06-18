9d362087db93874606e3a1a8eacadaca41a274a4
matrayu <northridge614@gmail.com>
2019-06-18 06:03:10 -0700

    Update increment-button.js

--- src/components/increment-button.js
+++ src/components/increment-button.js
@@ -1,5 +1,6 @@
 import React from 'react';
 
 export default function IncrementButton(props) {
-    return <button>Increment</button>;
+
+    return <button onClick={props.handleClick}>Increment</button>;
 }
